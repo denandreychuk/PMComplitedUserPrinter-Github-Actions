9f6b17ee9a7515cf430e4b67543c1436efc2b318
Den Andreychuk <denis.andrei4uk@yandex.ua>
2022-06-18 12:54:16 +0300

    Revert "Update deploy.yml"
    This reverts commit 17400e9e00b9c66f0189663d79c3fde281e847bb.

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -2,7 +2,7 @@ xcodebuild archive \
   -workspace PMUserPrinter.xcworkspace \
   -scheme PMUserPrinter \
   -sdk iphoneos \
-  -archivePath "output/xcframework_archives/ios_devices.xcarchive" \
+  -archivePath "xcframework_archives/ios_devices.xcarchive" \
   BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
   SKIP_INSTALL=NO
   
@@ -10,13 +10,11 @@ xcodebuild archive \
   -workspace PMUserPrinter.xcworkspace \
   -scheme PMUserPrinter \
   -sdk iphonesimulator \
-  -archivePath "output/xcframework_archives/ios_simulators.xcarchive" \
+  -archivePath "xcframework_archives/ios_simulators.xcarchive" \
   BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
   SKIP_INSTALL=NO
 
 xcodebuild -create-xcframework \
-  -framework output/xcframework_archives/ios_devices.xcarchive/Products/Library/Frameworks/PMUserPrinter.framework \
-  -framework output/xcframework_archives/ios_simulators.xcarchive/Products/Library/Frameworks/PMUserPrinter.framework \
+  -framework xcframework_archives/ios_devices.xcarchive/Products/Library/Frameworks/PMUserPrinter.framework \
+  -framework xcframework_archives/ios_simulators.xcarchive/Products/Library/Frameworks/PMUserPrinter.framework \
   -output PMUserPrinter.xcframework
-
-  zip -r output/PMComplitedUserPrinter.zip LICENSE PMUserPrinter.xcframework
