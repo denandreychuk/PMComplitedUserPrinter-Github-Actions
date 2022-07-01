cb3e74c31b00203811e864afdd697f1f473082e1
Den Andreychuk <denis.andrei4uk@yandex.ua>
2022-07-01 14:11:54 +0300

    upload script

---
+++ Scripts/upload.py
@@ -0,0 +1,22 @@
+from firebase_admin import initialize_app, credentials, storage
+from os import path
+import sys
+
+serviceAccount = "Users/runner/work/_temp/service-account.json"
+credentials = credentials.Certificate(serviceAccount)
+
+initialize_app(credentials, {"storageBucket": "user-printer.appspot.com"})
+
+# Upload file
+
+version = sys.argv[1]
+uploadPath = f"PMComplitedUserPrinter/{version}/PMComplitedUserPrinter.zip"
+filename = path.abspath(path.join(__file__, "../../output/PMComplitedUserPrinter.zip"))
+
+bucket = storage.bucket()
+blob = bucket.blob(uploadPath)
+blob.upload_from_filename(filename)
+
+blob.make_public()
+
+print(blob.public_url)
